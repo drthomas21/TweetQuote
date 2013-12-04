52ac1f959ebf1996b745f65b971ac0e44ff8b9c0
David Thomas <david.thomas214@yahoo.com>
2013-12-04 13:39:58 -0800

    Updated Example Page

--- index.php
+++ index.php
@@ -3,19 +3,10 @@ require_once('config.php');
 require_once('oauth/TweetQuote.php');
 
 $Instance = new TweetQuote($consumerKey,$consumerSecret);
-if($Instance->hasStoredOAuthToken()) {
-	$Instance->buildTwitterConnection();
-	if($_GET['tweet']) {
-		$arrTweets = $Instance->sendTweet($_GET['tweet']);
-	}
-} else {
-	$redirectUrl = $Instance->authorizeApp(true);
-	return;
-}
+$Instance->setCallbackUrl("http://{$_SERVER['SERVER_NAME']}/callback");
 
-
-if($_SERVER['REQUEST_URI'] != "/" && !preg_match("/\/\?tweet/", $_SERVER['REQUEST_URI'])) {
-	$request = strtolower($_SERVER['REQUEST_URI']);
+if($_SERVER['REQUEST_URI'] != "/" && stripos($_SERVER['REQUEST_URI'],"/?tweet") === false && stripos($_SERVER['REQUEST_URI'],"/?message") === false) {
+	$request = preg_replace("/\?.+/","",strtolower($_SERVER['REQUEST_URI']));
 	$action = preg_replace("/[^a-z\-]/", "", $request);
 	if(file_exists(dirname(__FILE__)."/view/{$action}.inc")) {
 		$file = (dirname(__FILE__)."/view/{$action}.inc");
@@ -23,6 +14,15 @@ if($_SERVER['REQUEST_URI'] != "/" && !preg_match("/\/\?tweet/", $_SERVER['REQUES
 		$file = (dirname(__FILE__)."/view/404.inc");
 	}
 } else {
+	if($Instance->hasStoredOAuthToken()) {
+		$Instance->buildTwitterConnection();
+		if($_GET['tweet']) {
+			$arrTweets = $Instance->sendTweet($_GET['tweet']);
+		}
+	} else {
+		$Instance->authorizeApp(true);
+		return;
+	}
 	$file = (dirname(__FILE__)."/view/index.inc");
 }
 include_once dirname(__FILE__).'/view/header.inc';
